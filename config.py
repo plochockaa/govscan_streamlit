04ac01176738d2867570245f7ae61f31f6d42afe
plochockaa <plochockaa@gmail.com>
2026-06-10 23:38:24 +0100

    feat: add config with distinct repos

---
+++ config.py
@@ -0,0 +1,12 @@
+ORGS: dict[str, list[str]] = {
+    "UK":          ["alphagov", "i-dot-ai", "co-cddo", "nhsengland",
+                    "DWPDigital", "hmrc", "ministryofjustice"],
+    "Canada":      ["canada-ca", "cds-snc", "statcan-algo"],
+    "Singapore":   ["govtechsg", "opengovsg", "datagovsg"],
+    "USA":         ["GSA", "18F", "uswds", "CDCgov", "USDS"],
+    "Australia":   ["AusDTO", "govau", "ServiceNSW"],
+    "New Zealand": ["ServiceInnovationLab", "nzgoal"],
+    "EU":          ["ec-europa", "etalab", "bundesministerium"],
+    "Estonia":     ["e-gov"],
+    "India":       ["egovernments", "mosip"],
+}
